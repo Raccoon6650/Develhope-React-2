4162ace956b79022b0852fe2571db33a58c7da26
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-14 12:03:00 +0200

    Svolto

--- esercizi_develhope/src/components/ClickTracker.js
+++ esercizi_develhope/src/components/ClickTracker.js
@@ -1,36 +1,21 @@
 import React, { useState } from "react";
 
-const ClickTracker = () => {
+const ClickTracker = (props) => {
   const [lastClick, setLastClick] = useState(null);
-  const [count, setCount] = useState(0);
 
   const ClickTracker = (name) => {
     setLastClick(name);
   };
 
-  const dec = () => setCount(count - 1);
-  const inc = () => setCount(count + 1);
-  const res = () => setCount(0);
-
-  const increment = () => {
-    ClickTracker("Increment");
-    inc();
-  };
-  const decrement = () => {
-    ClickTracker("Decrement");
-    dec();
-  };
-  const reset = () => {
-    ClickTracker("Reset");
-    res();
-  };
 
   return (
     <>
-      <h1>Count: {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      {['Tasto1', 'Tasto2', 'Tasto3'].map((el) => (
+        <button
+            onClick={() => ClickTracker(el)}>
+          {el}
+        </button>
+      ))}
       {lastClick && <h1>Last button clicked: {lastClick}</h1>}
     </>
   );
