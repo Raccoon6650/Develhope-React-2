0afaa97116bbba2a266d71e904da7dee9b6529af
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-17 19:01:54 +0200

    Svolto

--- esercizi_develhope/src/components/Login.js
+++ esercizi_develhope/src/components/Login.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useRef} from "react";
 
 const Login = (props) => {
-  //const [isDisabled, setIsDisabled] = useState(true);
   const email = useRef();
   const pass = useRef();
+  const button = useRef();
 
   /*useEffect(() => {
     setIsDisabled(email.current.value.length === 0 || pass.current.value.length === 0);
@@ -18,15 +18,20 @@ const Login = (props) => {
     setIsDisabled(!(em2 !== "" && pass2 !== ""));
   }, [email, pass]);*/
 
-
+  function isDisabled (){
+    const em2 = email.current.value.length
+    const pass2 = pass.current.value.length
+    const disabled = !(em2 !== 0 && pass2 !== 0) ? true : false 
+    button.current.disabled = disabled
+    console.log (disabled)}
 
 
     return (
         <>
         <div>
-        <input type= "email" placeholder="email" ref={email}/>
+        <input type= "email" placeholder="email" ref={email} onChange={isDisabled}/>
         </div>
-        <input type= "password" placeholder="password" ref={pass}/>
+        <input type= "password" placeholder="password" ref={pass} onChange={isDisabled}/>
                     {/*function isDisabled (){
                     const disabled = !(em2 !== "" && pass2 !== "" ) ? true : false 
                     return disabled}*/}
@@ -34,8 +39,8 @@ const Login = (props) => {
                                 await (em2, pass2)
                                 const disabled = !(em2 !== "" && pass2 !== "" ) ? true : false 
                                 return disabled}}>Submit</button>*/}
-        <button disabled = { email===0 && pass ===0 ? true : false }>Submit</button>
-        <button onClick={() =>{email.current.value = ""; pass.current.value = ""}}>Reset</button>
+        <button ref={button} disabled = "true">Submit</button>
+        <button onClick={() =>{email.current.value = ""; pass.current.value = ""; button.current.disabled = "true"}}>Reset</button>
         </>
     )}
 
