b2471019fb5132595d55143323222508f9c65432
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-17 18:38:14 +0200

    Svolto

--- esercizi_develhope/src/App.js
+++ esercizi_develhope/src/App.js
@@ -1,15 +1,42 @@
-//import ClickTracker from './components/ClickTracker';
-import Login from './components/Login';
+import React, { useState } from 'react';
+//import Login from './components/Login';
 import TodoList from './components/TodoList';
 //import CounterClick from './components/CounterClick';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState(""); 
+
+  const addItem = () => { 
+    setItems([...items, newItem]);
+    setNewItem("");
+  };
+
+  function handleKeyDown(e) {//aggiunto perché odio dover andare a premere il tastino
+    if (e.keyCode === 13) {
+      addItem();
+    }
+  }
+
+  const deleteItem = (i) => {
+    setItems(items.filter((item, index) => index !== i));
+  };
+
+  const resetList = () => { 
+    setItems([]);
+  };
+
+  const list = () => {
+
+    return items.map((item, i) => ( 
+    <li key={i} style={{listStyleType: 'numb'}}>{item} <button onClick={() => deleteItem(i)}>Delite</button></li>
+))}
+
   return (
     <div className="App">
       <header className="App-header">
         <>
-
-          <TodoList/>
+          <TodoList List ={list()} addItem= {addItem} resetList= {resetList} clickToSet = {(e) => setNewItem(e.target.value)} newItem = {newItem} onKeyDown={handleKeyDown}/>
         </>
       </header>
     </div>
--- esercizi_develhope/src/components/TodoList.js
+++ esercizi_develhope/src/components/TodoList.js
@@ -1,41 +1,27 @@
-import React, { useState } from "react";
 
-const TodoList = () => {
-  const [items, setItems] = useState([]); 
-  const [newItem, setNewItem] = useState(""); 
+const TodoList = (props) => {
 
-  const addItem = () => { 
-    setItems([...items, newItem]);
-    setNewItem("");
-  };
 
-  const resetList = () => { 
-    setItems([]);
-  };
-
-  const deleteItem = (i) => {
-    setItems(items.filter((item, index) => index !== i)); //per eliminare gli elementi con l'indice corrente
-  };
-  
 
 
   return (
     <div>
       <input
         type="text"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
+        value={props.newItem}
+        onChange={props.clickToSet}
+        onKeyDown = {props.onKeyDown}
       />
-      <button onClick={addItem}>Add Item</button>
-      <button onClick={resetList}>Reset</button>
+      <button onClick={props.addItem}>Add Item</button>
+      <button onClick={props.resetList}>Reset</button>
       <ul>
-        {items.map((item, i) => ( 
-          <li key={i} style={{listStyleType: 'numb'}}>{item} <button onClick={() => deleteItem(i)}>Delite</button></li>
-        ))}
+        {props.List}
       </ul>
     </div>
   );
 };
+
+
 
 export default TodoList;
 
