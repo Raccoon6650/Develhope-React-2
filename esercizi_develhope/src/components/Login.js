2b5eb46655e10d90b652e3ea63840e1d52303d65
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-17 19:06:47 +0200

    Svolto

--- esercizi_develhope/src/components/Login.js
+++ esercizi_develhope/src/components/Login.js
@@ -5,19 +5,6 @@ const Login = (props) => {
   const pass = useRef();
   const button = useRef();
 
-  /*useEffect(() => {
-    setIsDisabled(email.current.value.length === 0 || pass.current.value.length === 0);
-  }, [email.current.value, pass.current.value]);
-
-
-    const [isDisabled, setIsDisabled] = useState(false);
-
-  useEffect(() => {
-    const em2 = email.current.value.length;
-    const pass2 = pass.current.value.length;
-    setIsDisabled(!(em2 !== "" && pass2 !== ""));
-  }, [email, pass]);*/
-
   function isDisabled (){
     const em2 = email.current.value.length
     const pass2 = pass.current.value.length
@@ -29,27 +16,12 @@ const Login = (props) => {
     return (
         <>
         <div>
-        <input type= "email" placeholder="email" ref={email} onChange={isDisabled}/>
+        <input type= "email" placeholder="email" ref={email} onChange={isDisabled} autoFocus= "true" />
         </div>
         <input type= "password" placeholder="password" ref={pass} onChange={isDisabled}/>
-                    {/*function isDisabled (){
-                    const disabled = !(em2 !== "" && pass2 !== "" ) ? true : false 
-                    return disabled}*/}
-                                {/*<button disabled = {async function () {
-                                await (em2, pass2)
-                                const disabled = !(em2 !== "" && pass2 !== "" ) ? true : false 
-                                return disabled}}>Submit</button>*/}
         <button ref={button} disabled = "true">Submit</button>
         <button onClick={() =>{email.current.value = ""; pass.current.value = ""; button.current.disabled = "true"}}>Reset</button>
         </>
     )}
-
-    /*Login.defaultProps = {
-        disabled: {isDisabled(){
-            const em2 = email.current.value.length
-            const pass2 = pass.current.value.length
-            const disabled = !(em2 !== "" && pass2 !== "" ) ? true : false 
-            return disabled}}
-    }*/
 
     export default Login
