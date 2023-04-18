e52a2d2bada816874d0629818460b15d5c72db31
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-18 17:55:52 +0200

    da_finire

--- esercizi_develhope/src/App.jsx
+++ esercizi_develhope/src/App.jsx
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CounterClick from './components/CounterClick';
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
-
-  function collateral () {
-    return (
-    <>
-    { count }
-    </>
-  )}
-  
   return (
-    <CounterClick onCounterChange={collateral} count = {count} increment ={increment} decrement ={decrement} reset = {reset}/>
+    <CounterClick/>
   );
 }
 
--- esercizi_develhope/src/components/CounterClick.js
+++ esercizi_develhope/src/components/CounterClick.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 //import ClickTracker from "./ClickTracker";
 
 const CounterClick = (props) => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
 
   return (
     <>
-      <h1 onChange={props.onCounterChange}>Count: {props.count}</h1>
-      <button onClick={props.increment}>Increment</button>
-      <button onClick={props.decrement}>Decrement</button>
-      <button onClick={props.reset}>Reset</button>
+      <h1>Count: { count }</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 };
