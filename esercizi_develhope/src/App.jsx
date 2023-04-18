5f2b1c5d2f716fd5a28978ed699e36dc46a3691f
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-19 00:23:25 +0200

    fatto

--- esercizi_develhope/src/App.jsx
+++ esercizi_develhope/src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-//import Counter from './components/Counter';
-import LoginForm from './components/LoginForm';
-//import Counter from './components/CounterClick';
-//import GitHubUser from './components/GitHubUser';
-//import GHUList from './components/GHUList';
+import Counter from './components/Counter';
+
 
 function App() {
     return(
-    <LoginForm/>
+    <Counter/>
     )
 }
 
