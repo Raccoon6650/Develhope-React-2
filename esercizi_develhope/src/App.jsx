854a7959720708ffe5f63f0b8dcfc8f45530801a
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-18 18:18:14 +0200

    fatto

--- esercizi_develhope/src/App.jsx
+++ esercizi_develhope/src/App.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import CounterClick from './components/CounterClick';
+import Counter from './components/CounterClick';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  useEffect(function change () {
 
+    return(
+      console.log({count}))
+},[count])
   return (
-    <CounterClick/>
+    <Counter increment={increment} count= {count}/>
   );
 }
 
--- esercizi_develhope/src/components/CounterClick.js
+++ esercizi_develhope/src/components/CounterClick.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
-//import ClickTracker from "./ClickTracker";
+import React, { useEffect, useState } from "react";
 
-const CounterClick = (props) => {
+const Counter = function Counter() {
   const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(count => count + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
 
   return (
     <>
-      <h1>Count: { count }</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <h1>{count}</h1>
     </>
   );
-};
+}
 
-export default CounterClick;
+export default Counter;
