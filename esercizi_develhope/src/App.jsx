8021f480a2853881aac3cb422eafa187a1cba3be
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-18 15:31:43 +0200

    fatto

--- esercizi_develhope/src/App.jsx
+++ esercizi_develhope/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import Login from './components/Login';
+import React, { useState } from 'react';
+import CounterClick from './components/CounterClick';
 
 function App() {
+  const [count, setCount] = useState(0);
 
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  function collateral () {
+    return (
+    <>
+    { count }
+    </>
+  )}
   
   return (
-    <Login/>
+    <CounterClick onCounterChange={collateral} count = {count} increment ={increment} decrement ={decrement} reset = {reset}/>
   );
 }
 
--- esercizi_develhope/src/components/CounterClick.js
+++ esercizi_develhope/src/components/CounterClick.js
@@ -1,19 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 //import ClickTracker from "./ClickTracker";
 
-const CounterClick = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const reset = () => setCount(0);
+const CounterClick = (props) => {
 
   return (
     <>
-      <h1>Count: {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <h1 onChange={props.onCounterChange}>Count: {props.count}</h1>
+      <button onClick={props.increment}>Increment</button>
+      <button onClick={props.decrement}>Decrement</button>
+      <button onClick={props.reset}>Reset</button>
     </>
   );
 };
