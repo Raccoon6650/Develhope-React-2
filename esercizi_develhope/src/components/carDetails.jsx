81012efa882b483115a4743aa835858fbc68a2e6
Raccoon6650 <mario.caruso098@gmail.com>
2023-04-24 11:56:16 +0200

    fatto

--- esercizi_develhope/src/components/carDetails.jsx
+++ esercizi_develhope/src/components/carDetails.jsx
@@ -4,10 +4,14 @@ const CarDetails = (props) => {
 const model = useRef()
 const year = useRef()
 const color = useRef()
+const button = useRef()
 
 function hendelClick (e){
     e.preventDefault(); 
     console.log(model.current.value, year.current.value, color.current.value)
+    model.current.value= props.InitialData.model;
+    year.current.value = props.InitialData.year;
+    color.current.value = props.InitialData.color;
 }
 
 
@@ -15,12 +19,12 @@ return(
     <>
         <form>
             <div>
-            <input type= "text" defaultValue={props.InitialData.model} ref={model} autoFocus= "true" /><br/>
+            <input type= "text" defaultValue={props.InitialData.model} ref={model} autoFocus /><br/>
             <input type= "text" defaultValue={props.InitialData.year} ref={year} /><br/>
             <input type= "text" defaultValue={props.InitialData.color} ref={color} />
             </div>
-            <button onClick={hendelClick}>Submit</button>
-            <button onClick={() =>{ model.current.value = ""; year.current.value = ""; color.current.value = ""; model.current.focus()}}>Reset</button>
+            <button ref={button} onClick={hendelClick}>Submit</button>
+            <button onClick={() =>{ model.current.value = props.InitialData.model; year.current.value = props.InitialData.year; color.current.value = props.InitialData.color; model.current.focus()}}>Reset</button>
         </form>
     </>
 )
